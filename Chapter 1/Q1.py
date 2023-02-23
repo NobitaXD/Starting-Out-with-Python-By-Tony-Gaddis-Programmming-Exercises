fcc1dee2ed38d1c0c0880358d7ef227b22b38f77
Muhammad Saim <100432816+NobitaXD@users.noreply.github.com>
2023-02-23 23:00:44 +0500

    Create Q1.py

---
+++ Chapter 1/Q1.py
@@ -0,0 +1,17 @@
+# Muhammad Saim
+# https://github.com/NobitaXD
+name = "Muhammad Saim"
+address = "XYZ Lane "
+city = "Islamabad"
+state = "Punjab"
+zip = "62004"
+number = "031111111111"
+major = "CS"
+
+print("Name: ", name)
+print("Address: ", address)
+print("City: ", city)
+print("State", state)
+print("ZIP: ", zip)
+print("Number: ", number)
+print("Major: ", major)
