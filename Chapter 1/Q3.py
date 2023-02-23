16d2a487b499800624f2ec05822f23b235e2254f
Muhammad Saim <100432816+NobitaXD@users.noreply.github.com>
2023-02-23 23:26:36 +0500

    Create Q3.py

---
+++ Chapter 1/Q3.py
@@ -0,0 +1,10 @@
+# Muhammad Saim
+# https://github.com/NobitaXD
+
+pounds = 0.454
+
+mass = int(input("Enter Mass in Pounds: "))
+
+mass_kilo = mass * pounds
+
+print("Mass in Kilograms: ", mass_kilo)
